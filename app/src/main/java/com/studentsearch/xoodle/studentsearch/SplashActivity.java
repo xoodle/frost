0c122f25a8eb6f9fcec7c3ba1b5dc9473be101c1
Aryan Sharma <aryanshar@github.com>
2017-07-12 03:45:01 +0530

    Added splashscreen animation

--- app/src/main/java/com/studentsearch/xoodle/studentsearch/SplashActivity.java
+++ app/src/main/java/com/studentsearch/xoodle/studentsearch/SplashActivity.java
@@ -1,21 +1,46 @@
 package com.studentsearch.xoodle.studentsearch;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 /**
  * Created by aryan on 7/6/17.
  */
 
 public class SplashActivity extends AppCompatActivity {
-
+  ProgressBar mprogressBar;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.splashscreen);
+    Animation anim1 = AnimationUtils.loadAnimation(this,R.anim.anim_down);
+    ImageView img =(ImageView)findViewById(R.id.imageView);
+    img.setAnimation(anim1);
 
-    Intent intent = new Intent(this, MainActivity.class);
-    startActivity(intent);
-    finish();
+    mprogressBar = (ProgressBar) findViewById(R.id.progressBar);
+    ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, 100);
+    anim.setDuration(4000);
+    anim.setInterpolator(new DecelerateInterpolator());
+    anim.start();
+
+    Handler handler = new Handler();
+
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
+
+      }
+    },3000);
   }
 }
